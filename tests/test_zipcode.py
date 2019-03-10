2fb9ca67ac9fad163b9f2641c05e706d6feae081
Lucas Simon <lucassrod@gmail.com>
2019-03-10 18:51:58 -0300

    Improve tests of search method

--- tests/test_zipcode.py
+++ tests/test_zipcode.py
@@ -1,12 +1,10 @@
 import pytest
-import requests
+
 
 from hub_cep.zipcode import ZipCode, AbstractZipCode
 from hub_cep.exceptions import ZipcodeError
 
-from .test_providers import ZIPCODE, INVALID_ZIPCODE
-
-MOCK_RESULT = {'error': False, 'message': 'Success.', 'data': {'zip_code': '78048-000', 'address': 'Avenida Miguel Sutil', 'number': '', 'info': '', 'district': 'Alvorada', 'city': 'Cuiabá', 'state': 'MT', 'country': 'BRA'}}
+from .test_providers import ZIPCODE, INVALID_ZIPCODE, TestViacep, TestPostmon, TestCepaberto
 
 
 class TestAbstractZipcode:
@@ -27,6 +25,14 @@ class TestAbstractZipcode:
 
 
 class TestZipcode:
+
+    FAKE_SUCCESS_RESPONSE = {
+        'error': False, 'message': 'Success.',
+        'data': {
+            'zip_code': '78048-000', 'address': 'Avenida Miguel Sutil', 'number': '',
+            'info': '', 'district': 'Alvorada', 'city': 'Cuiabá', 'state': 'MT', 'country': 'BRA'
+        }
+    }
 
     def test_zipcode_getter(self):
         client = ZipCode(ZIPCODE)
@@ -49,11 +55,11 @@ class TestZipcode:
 
         assert e.value.args[0] == 'Zipcode invalid.'
 
-    def test_fetch_sucess(self):
+    def test_fetch_success(self):
         client = ZipCode(ZIPCODE)
         status_code, result = client.search()
         assert status_code == 200
-        assert result == MOCK_RESULT
+        assert result == TestZipcode.FAKE_SUCCESS_RESPONSE
 
     def test_fetch_with_error(self):
         client = ZipCode(INVALID_ZIPCODE)
@@ -64,3 +70,40 @@ class TestZipcode:
 
         assert status_code == 422
         assert result == {'error': True, 'message': 'Zip code not found.'}
+
+    def test_fetch_success_from_postmon_provider(self, requests_mock):
+        fake_response = {
+            'cep': '78048-000',
+            'logradouro': 'Avenida Miguel Sutil',
+            'bairro': 'Alvorada',
+            'cidade': 'Cuiabá',
+            'estado': 'MT',
+        }
+
+        requests_mock.get(TestViacep.FAKE_URL, status_code=406)
+        requests_mock.get(TestPostmon.FAKE_URL, json=fake_response, status_code=200)
+        client = ZipCode(ZIPCODE)
+        status_code, result = client.search()
+        assert status_code == 200
+        assert result == TestZipcode.FAKE_SUCCESS_RESPONSE
+
+    def test_fetch_success_from_cepaberto_provider(self, requests_mock):
+        fake_response = {
+            'cep': '78048-000',
+            'logradouro': 'Avenida Miguel Sutil, de 5799/5800 a 7887/7888',
+            'bairro': 'Consil',
+            'cidade': {'nome': 'Cuiabá'},
+            'estado': {'sigla': 'MT'},
+        }
+
+        TestZipcode.FAKE_SUCCESS_RESPONSE['data']['district'] = fake_response['bairro']
+        TestZipcode.FAKE_SUCCESS_RESPONSE['data']['address'] = fake_response['logradouro']
+
+        requests_mock.get(TestViacep.FAKE_URL, status_code=406)
+        requests_mock.get(TestPostmon.FAKE_URL, status_code=406)
+        requests_mock.get(TestCepaberto.FAKE_URL, json=fake_response, status_code=200)
+        client = ZipCode(ZIPCODE)
+        status_code, result = client.search()
+
+        assert status_code == 200
+        assert result == TestZipcode.FAKE_SUCCESS_RESPONSE
